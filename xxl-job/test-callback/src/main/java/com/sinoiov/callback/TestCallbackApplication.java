27167b766c1fe7b0a074f0c370c44362a3d6a816
guoyangyang <guoyangyang@sinoiov.com>
2020-05-27 18:06:11 +0800

    删除XXL-JOB中无用文件

---
+++ xxl-job/test-callback/src/main/java/com/sinoiov/callback/TestCallbackApplication.java
@@ -0,0 +1,14 @@
+package com.sinoiov.callback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class TestCallbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCallbackApplication.class, args);
+    }
+
+}
